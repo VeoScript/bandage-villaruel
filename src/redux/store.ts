095e93f1c224064bc52ba98c5a08855846aa9bbe
Villaruel Jerome <jeromevillaruel1998@gmail.com>
2024-01-03 20:20:52 +0800

    feat: setup rtk-query

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,21 @@
+"use client";
+
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
-export const makeStore = () => {
-  return configureStore({
-    reducer: {},
-  });
-};
+import testingReducer from "./slices/testing/testingSlice";
+import productsReducer from "./slices/products/queries";
 
-// Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>;
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore["getState"]>;
-export type AppDispatch = AppStore["dispatch"];
+export const store = configureStore({
+  reducer: {
+    testing: testingReducer,
+    [productsReducer.reducerPath]: productsReducer.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsReducer.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+setupListeners(store.dispatch);
