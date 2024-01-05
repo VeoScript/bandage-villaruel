8bd582655da47c27b39d2dc53c5fb5a9b3977310
Villaruel Jerome <jeromevillaruel1998@gmail.com>
2024-01-05 15:59:50 +0800

    feat: add responsive layout for navbar

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,13 +1,20 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import Logo from "./Logo";
+
+import clsx from "clsx";
 
 import CartMenu from "./menus/CartMenu";
 import WishlistMenu from "./menus/WishlistMenu";
 
 export default function NavBar(): JSX.Element {
+  const [isShowMenu, setIsShowMenu] = useState<boolean>(false);
+
   return (
     <nav className="flex flex-col w-full">
-      <div className="flex flex-col items-center w-full text-white bg-accent-3">
+      <div className="hidden md:flex flex-col items-center w-full px-3 text-white bg-accent-3">
         <div className="flex flex-row items-center justify-between w-full max-w-6xl py-3">
           <div className="flex flex-row items-center justify-start w-full gap-x-5">
             <h3 className="flex flex-row items-center gap-x-1 font-semibold text-[14px]">
@@ -128,44 +135,119 @@ export default function NavBar(): JSX.Element {
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center w-full text-accent-2 bg-accent-1">
-        <div className="flex flex-row items-center justify-between w-full max-w-6xl py-3">
-          <div className="flex flex-row items-center justify-start w-full gap-x-[10rem]">
-            <Logo />
-            <ul className="flex flex-row items-center justify-center gap-x-5">
+      <div className="flex flex-col items-center w-full p-5 md:p-3 text-accent-2 bg-accent-1">
+        <div className="flex flex-row items-center justify-between w-full max-w-full md:max-w-6xl py-3">
+          <div className="flex flex-col md:flex-row items-center w-full gap-x-5 md:gap-x-[10rem] gap-y-10 md:gap-y-0">
+            <div className="flex flex-row items-center justify-between w-full">
+              <Logo />
+              <div className="flex md:hidden flex-row items-center justify-end gap-x-5">
+                <button type="button" aria-label="Search">
+                  <svg
+                    className="w-5 h-5 fill-current text-accent-2"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g clipPath="url(#clip0_540_804)">
+                      <path d="M11.7426 10.3441C12.7109 9.02279 13.1446 7.38459 12.9569 5.75725C12.7692 4.12991 11.974 2.63344 10.7304 1.56723C9.48671 0.501022 7.88634 -0.0562959 6.24943 0.00677721C4.61252 0.0698504 3.05978 0.748663 1.90186 1.90741C0.743944 3.06615 0.0662432 4.61938 0.00434136 6.25633C-0.0575605 7.89329 0.500902 9.49326 1.568 10.7361C2.6351 11.979 4.13214 12.7732 5.75961 12.9597C7.38709 13.1462 9.02497 12.7113 10.3456 11.7421H10.3446C10.3746 11.7821 10.4066 11.8201 10.4426 11.8571L14.2926 15.7071C14.4801 15.8947 14.7345 16.0002 14.9997 16.0003C15.265 16.0004 15.5195 15.8951 15.7071 15.7076C15.8947 15.5201 16.0002 15.2657 16.0003 15.0005C16.0004 14.7352 15.8951 14.4807 15.7076 14.2931L11.8576 10.4431C11.8218 10.4069 11.7834 10.3735 11.7426 10.3431V10.3441ZM12.0006 6.5001C12.0006 7.22237 11.8583 7.93757 11.5819 8.60486C11.3055 9.27215 10.9004 9.87847 10.3897 10.3892C9.87896 10.8999 9.27264 11.305 8.60535 11.5814C7.93806 11.8578 7.22286 12.0001 6.50059 12.0001C5.77832 12.0001 5.06312 11.8578 4.39583 11.5814C3.72854 11.305 3.12223 10.8999 2.61151 10.3892C2.10078 9.87847 1.69566 9.27215 1.41926 8.60486C1.14285 7.93757 1.00059 7.22237 1.00059 6.5001C1.00059 5.04141 1.58006 3.64246 2.61151 2.61101C3.64296 1.57956 5.0419 1.0001 6.50059 1.0001C7.95928 1.0001 9.35823 1.57956 10.3897 2.61101C11.4211 3.64246 12.0006 5.04141 12.0006 6.5001Z" />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_540_804">
+                        <rect
+                          width="16.0006"
+                          height="16.0003"
+                          fill="white"
+                          transform="translate(-0.000305176)"
+                        />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                </button>
+                <CartMenu
+                  className="flex flex-row items-center gap-x-1 text-accent-2 outline-none"
+                  iconClassName="w-5 h-5 fill-current"
+                />
+                <WishlistMenu
+                  className="flex flex-row items-center gap-x-1 text-accent-2 outline-none"
+                  iconClassName="w-5 h-5 fill-current"
+                />
+                <button type="button" aria-label="Menu" onClick={() => setIsShowMenu(!isShowMenu)}>
+                  <svg
+                    className="w-5 h-5 fill-current text-accent-2"
+                    width="24"
+                    height="14"
+                    viewBox="0 0 24 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M0.571472 0H23.4286V2.28571H0.571472V0ZM6.28576 5.71429H23.4286V8H6.28576V5.71429ZM13.4286 11.4286H23.4286V13.7143H13.4286V11.4286Z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <ul
+              className={clsx(
+                isShowMenu ? "flex" : "hidden md:flex",
+                "flex-col md:flex-row items-center justify-center gap-x-0 md:gap-x-5 gap-y-10 md:gap-y-0",
+              )}
+            >
               <li>
-                <Link className="font-semibold text-sm leading-[0.2px] text-neutral-500" href="/">
+                <Link
+                  className="font-semibold text-[30px] md:text-[14px] leading-[0.2px] text-neutral-500"
+                  href="/"
+                  onClick={() => setIsShowMenu(false)}
+                >
                   Home
                 </Link>
               </li>
-              <li>
-                <Link className="font-semibold text-sm leading-[0.2px] text-neutral-500" href="/">
+              <li className="hidden md:flex">
+                <Link
+                  className="font-semibold text-[30px] md:text-[14px] leading-[0.2px] text-neutral-500"
+                  href="/"
+                  onClick={() => setIsShowMenu(false)}
+                >
                   Shop
                 </Link>
               </li>
-              <li>
-                <Link className="font-semibold text-sm leading-[0.2px] text-neutral-500" href="/">
+              <li className="hidden md:flex">
+                <Link
+                  className="font-semibold text-[30px] md:text-[14px] leading-[0.2px] text-neutral-500"
+                  href="/"
+                  onClick={() => setIsShowMenu(false)}
+                >
                   About
                 </Link>
               </li>
               <li>
-                <Link className="font-semibold text-sm leading-[0.2px] text-neutral-500" href="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="font-semibold text-sm leading-[0.2px] text-neutral-500" href="/">
+                <Link
+                  className="font-semibold text-[30px] md:text-[14px] leading-[0.2px] text-neutral-500"
+                  href="/"
+                  onClick={() => setIsShowMenu(false)}
+                >
                   Blog
                 </Link>
               </li>
               <li>
-                <Link className="font-semibold text-sm leading-[0.2px] text-neutral-500" href="/">
+                <Link
+                  className="font-semibold text-[30px] md:text-[14px] leading-[0.2px] text-neutral-500"
+                  href="/"
+                  onClick={() => setIsShowMenu(false)}
+                >
                   Contact
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="font-semibold text-[30px] md:text-[14px] leading-[0.2px] text-neutral-500"
+                  href="/"
+                  onClick={() => setIsShowMenu(false)}
+                >
+                  Pages
                 </Link>
               </li>
             </ul>
           </div>
-          <div className="flex flex-row items-center justify-end w-full gap-x-5">
+          <div className="hidden md:flex flex-row items-center justify-end w-full gap-x-5">
             <Link
               href="/"
               className="flex flex-row items-center gap-x-1 font-semibold leading-[0.2px] text-[14px] text-accent-4"
--- src/components/ui/menus/WishlistMenu.tsx
+++ src/components/ui/menus/WishlistMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Popover } from "@headlessui/react";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import clsx from "clsx";
@@ -9,29 +10,40 @@ import type { RootState } from "~/redux/store";
 import { useSelector, useDispatch } from "react-redux";
 import { removeWishlist, clearWishlists } from "~/redux/slices/features/wishlistSlice";
 
-export default function WishlistMenu(): JSX.Element {
+interface WishlistMenuProps {
+  className?: string;
+  iconClassName?: string;
+}
+
+export default function WishlistMenu({ className, iconClassName }: WishlistMenuProps): JSX.Element {
+  const pathname = usePathname();
+
   const dispatch = useDispatch();
 
   const { wishlist_count, wishlists } = useSelector((state: RootState) => state.wishlist);
 
   return (
-    <Popover className="relative">
-      <Popover.Button className="flex flex-row items-center gap-x-1 text-accent-4 outline-none">
+    <Popover className="static md:relative">
+      <Popover.Button
+        className={
+          className ? className : "flex flex-row items-center gap-x-1 text-accent-4 outline-none"
+        }
+      >
         <div className="relative">
-          {wishlists.length > 0 && (
-            <span className="absolute -top-1 -right-1 w-2 h-2 rounded-full bg-red-600" />
-          )}
+          <span
+            className={clsx(
+              wishlists.length == 0 ? "hidden" : "flex",
+              "absolute -top-1 -right-1 w-2 h-2 rounded-full bg-red-600",
+            )}
+          />
           <svg
-            className="w-4 h-4"
+            className={iconClassName ? iconClassName : "w-4 h-4 fill-current"}
             viewBox="0 0 16 16"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
             <g clipPath="url(#clip0_540_815)">
-              <path
-                d="M8.0002 2.74805L7.2832 2.01105C5.6002 0.281049 2.5142 0.878049 1.4002 3.05305C0.8772 4.07605 0.7592 5.55305 1.7142 7.43805C2.6342 9.25305 4.5482 11.427 8.0002 13.795C11.4522 11.427 13.3652 9.25305 14.2862 7.43805C15.2412 5.55205 15.1242 4.07605 14.6002 3.05305C13.4862 0.878049 10.4002 0.280049 8.7172 2.01005L8.0002 2.74805ZM8.0002 15C-7.3328 4.86805 3.2792 -3.03995 7.8242 1.14305C7.8842 1.19805 7.9432 1.25505 8.0002 1.31405C8.05662 1.2551 8.11533 1.19839 8.1762 1.14405C12.7202 -3.04195 23.3332 4.86705 8.0002 15Z"
-                fill="#23A6F0"
-              />
+              <path d="M8.0002 2.74805L7.2832 2.01105C5.6002 0.281049 2.5142 0.878049 1.4002 3.05305C0.8772 4.07605 0.7592 5.55305 1.7142 7.43805C2.6342 9.25305 4.5482 11.427 8.0002 13.795C11.4522 11.427 13.3652 9.25305 14.2862 7.43805C15.2412 5.55205 15.1242 4.07605 14.6002 3.05305C13.4862 0.878049 10.4002 0.280049 8.7172 2.01005L8.0002 2.74805ZM8.0002 15C-7.3328 4.86805 3.2792 -3.03995 7.8242 1.14305C7.8842 1.19805 7.9432 1.25505 8.0002 1.31405C8.05662 1.2551 8.11533 1.19839 8.1762 1.14405C12.7202 -3.04195 23.3332 4.86705 8.0002 15Z" />
             </g>
             <defs>
               <clipPath id="clip0_540_815">
@@ -40,94 +52,108 @@ export default function WishlistMenu(): JSX.Element {
             </defs>
           </svg>
         </div>
-        <span className="font-semibold">{wishlist_count}</span>
+        <p className="font-semibold">{wishlist_count}</p>
       </Popover.Button>
 
-      <Popover.Panel className="absolute z-10 top-10 right-0 flex flex-col items-start w-[30rem] overflow-hidden rounded-xl shadow-md border border-neutral-200 bg-accent-1">
-        <div className="flex flex-row items-center justify-start w-full p-3">
+      <Popover.Panel className="absolute z-30 top-0 md:top-10 right-0 flex flex-col items-start justify-between md:justify-start w-screen h-screen md:w-[30rem] md:h-auto overflow-auto md:overflow-hidden rounded-xl shadow-md border border-neutral-200 bg-accent-1">
+        <div className="flex flex-row items-center justify-between w-full p-3">
           <h2 className="font-normal text-[12px] text-neutral-500">Your Wishlist</h2>
-        </div>
-        {wishlists.length == 0 ? (
-          <div className="flex flex-col items-center justify-center w-full h-[10rem] gap-y-3">
+          <Popover.Button as={Link} href={pathname} className="flex md:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="w-10 h-10 text-neutral-300"
+              className="w-6 h-6"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-              />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
-            <p className="font-bold text-sm text-neutral-300">No Saved Products Yet</p>
-          </div>
-        ) : (
-          <>
-            {wishlists.map(
-              (wishlist: {
-                id: string;
-                image: string;
-                title: string;
-                price: string;
-                quantity: number;
-              }) => (
-                <div
-                  key={wishlist.id}
-                  className="flex flex-row items-start justify-between w-full p-3 gap-x-5"
-                >
-                  <div className="relative w-[50px] h-[50px] overflow-hidden rounded-md">
-                    <Image
-                      fill
-                      className="object-cover"
-                      src={wishlist.image}
-                      alt="Product thumbnail"
-                      sizes="(max-width: 50px) 100vw, (max-width: 50px) 50vw, 33vw"
-                      placeholder="blur"
-                      blurDataURL={wishlist.image}
-                    />
-                  </div>
-                  <div className="flex flex-1 flex-col items-start gap-y-1">
-                    <Link
-                      href={`/products/${wishlist.id}`}
-                      className="font-bold text-[16px] line-clamp-1 capitalize"
-                    >
-                      {wishlist.title}
-                    </Link>
-                    <h2 className="font-bold text-[14px] text-neutral-500">
-                      $ {Number(wishlist.price).toFixed(2)}
-                    </h2>
-                  </div>
-                  <button
-                    type="button"
-                    className="flex flex-row items-center justify-center w-10 h-10 outline-none rounded-full border border-neutral-200 bg-accent-1 hover:opacity-50"
-                    onClick={() => {
-                      dispatch(removeWishlist(wishlist.id as unknown as number));
-                    }}
+          </Popover.Button>
+        </div>
+        <div className="flex flex-col items-center w-full h-full">
+          {wishlists.length == 0 ? (
+            <div className="flex flex-col items-center justify-center w-full h-[10rem] gap-y-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-10 h-10 text-neutral-300"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                />
+              </svg>
+              <p className="font-bold text-sm text-neutral-300">No Saved Products Yet</p>
+            </div>
+          ) : (
+            <>
+              {wishlists.map(
+                (wishlist: {
+                  id: string;
+                  image: string;
+                  title: string;
+                  price: string;
+                  quantity: number;
+                }) => (
+                  <div
+                    key={wishlist.id}
+                    className="flex flex-row items-start justify-between w-full p-3 gap-x-5"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5 text-neutral-500"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    <div className="relative w-[50px] h-[50px] overflow-hidden rounded-md">
+                      <Image
+                        fill
+                        className="object-cover"
+                        src={wishlist.image}
+                        alt="Product thumbnail"
+                        sizes="(max-width: 50px) 100vw, (max-width: 50px) 50vw, 33vw"
+                        placeholder="blur"
+                        blurDataURL={wishlist.image}
                       />
-                    </svg>
-                  </button>
-                </div>
-              ),
-            )}
-          </>
-        )}
+                    </div>
+                    <div className="flex flex-1 flex-col items-start gap-y-1">
+                      <Link
+                        href={`/products/${wishlist.id}`}
+                        className="font-bold text-[16px] line-clamp-1 capitalize"
+                      >
+                        {wishlist.title}
+                      </Link>
+                      <h2 className="font-bold text-[14px] text-neutral-500">
+                        $ {Number(wishlist.price).toFixed(2)}
+                      </h2>
+                    </div>
+                    <button
+                      type="button"
+                      className="flex flex-row items-center justify-center w-10 h-10 outline-none rounded-full border border-neutral-200 bg-accent-1 hover:opacity-50"
+                      onClick={() => {
+                        dispatch(removeWishlist(wishlist.id as unknown as number));
+                      }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5 text-neutral-500"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                ),
+              )}
+            </>
+          )}
+        </div>
         <div className="flex flex-row items-center justify-between w-full p-3 gap-x-3 border-t border-neutral-200">
           <div className="flex flex-row items-center gap-x-2">
             <button
